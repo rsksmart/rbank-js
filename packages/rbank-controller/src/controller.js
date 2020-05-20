15926140bc3a120a0205ff3b8fd41e1f7221fe91
Mesi Rendon <amrendonsa@unal.edu.co>
2020-05-19 23:58:55 -0500

    Improving tests on controller package
            I've edited several tests for the controller. What I did was to
            improve the way the beforeEach hook was being executed. I also
            improved several calls within the controller class.
    
            I'm still missing the rollup configuration so I can send common
            methods to the utils package.
    
            I'm getting a weird behavior on the `addMarket` method, it
            throws an error that I can identify yet.

--- packages/rbank-controller/src/controller.js
+++ packages/rbank-controller/src/controller.js
@@ -3,6 +3,15 @@ import Web3 from 'web3';
 
 const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
 
+const send = (signature) => new Promise((resolve, reject) => {
+  web3.eth.getAccounts()
+    .then(([from]) => [from, signature.estimateGas({ from })])
+    .then(promises => Promise.all(promises))
+    .then(([from, gas]) => signature.send({ from, gas }))
+    .then(resolve)
+    .catch(reject);
+});
+
 /**
  * Controller handler
  */
@@ -33,6 +42,54 @@ export default class Controller {
       this._instance.methods.liquidationFactor()
         .call()
         .then(liquidationFactor => Number(liquidationFactor))
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  get eventualMarketListSize() {
+    return new Promise((resolve, reject) => {
+      this._instance.methods.marketListSize()
+        .call()
+        .then(marketListSize => Number(marketListSize))
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  setCollateralFactor(collateralFactor) {
+    return new Promise((resolve, reject) => {
+      send(this._instance.methods.setCollateralFactor(collateralFactor))
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  setLiquidationFactor(liquidationFactor) {
+    return new Promise((resolve, reject) => {
+      send(this._instance.methods.setLiquidationFactor(liquidationFactor))
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  addMarket(marketAddress) {
+    return new Promise((resolve, reject) => {
+      send(this._instance.methods.addMarket(marketAddress))
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  static create() {
+    return new Promise((resolve, reject) => {
+      const controller = new web3.eth.Contract(ControllerContract.abi);
+      const deploy = controller.deploy({ data: ControllerContract.bytecode });
+      web3.eth.getAccounts()
+        .then(([from]) => [from, deploy.estimateGas({ from })])
+        .then(result => Promise.all(result))
+        .then(([from, gas]) => deploy.send({ from, gas }))
+        .then(instance => instance._address)
         .then(resolve)
         .catch(reject);
     });
--- packages/rbank-controller/test/controller.spec.js
+++ packages/rbank-controller/test/controller.spec.js
@@ -1,17 +1,21 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import Web3 from 'web3';
+import FaucetTokenContract from '../../../dependencies/DeFiProt/build/contracts/FaucetToken.json';
 import Controller from '../src/controller';
+
+const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
 
 chai.use(chaiAsPromised);
 
 const { expect } = chai;
 
 describe('Controller handler', () => {
+  let controller;
+  beforeEach(async () => {
+    controller = new Controller(await Controller.create());
+  });
   context('Initialization', () => {
-    let controller;
-    beforeEach(() => {
-      controller = new Controller('0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab');
-    });
     it('should get an error when initializing without controller address', () => {
       expect(new Controller()).to.be.an('error');
     });
@@ -28,6 +32,64 @@ describe('Controller handler', () => {
       return controller.eventualLiquidationFactor
         .then(collateralFactor => {
           expect(collateralFactor).to.eq(0);
+        });
+    });
+    it('should allow the controller owner to set a new collateral factor', () => {
+      return controller.setCollateralFactor(2)
+        .then(() => controller.eventualCollateralFactor)
+        .then(collateralFactor => {
+          expect(collateralFactor).to.eq(2);
+        });
+    });
+    it('should allow the controller owner to set a new liquidation factor', () => {
+      return controller.setLiquidationFactor(3)
+        .then(() => controller.eventualLiquidationFactor)
+        .then(liquidationFactor => {
+          expect(liquidationFactor).to.eq(3);
+        });
+    });
+  });
+  context('Market management', () => {
+    let token1;
+    let token2;
+    let token3;
+    beforeEach(async () => {
+
+      const token = new web3.eth.Contract(FaucetTokenContract.abi);
+      const [owner, from] = await web3.eth.getAccounts();
+
+      const deploy1 = token.deploy({
+        data: FaucetTokenContract.bytecode,
+        arguments: [10000, 'TOK1', 0, 'TOK1'],
+      });
+      const gas1 = await deploy1.estimateGas({ from: owner });
+      token1 = await deploy1.send({ from: owner, gas: gas1 });
+
+      const deploy2 = token.deploy({
+        data: FaucetTokenContract.bytecode,
+        arguments: [10000, 'TOK2', 0, 'TOK2'],
+      });
+      const gas2 = await deploy2.estimateGas({ from });
+      token2 = await deploy2.send({ from, gas: gas2 });
+
+      const deploy3 = token.deploy({
+        data: FaucetTokenContract.bytecode,
+        arguments: [10000, 'TOK3', 0, 'TOK3'],
+      });
+      const gas3 = await deploy3.estimateGas({ from });
+      token3 = await deploy3.send({ from, gas: gas3 });
+    });
+    it('should have zero markets in the beginning', () => {
+      return controller.eventualMarketListSize
+        .then(marketListSize => {
+          expect(marketListSize).to.eq(0);
+        });
+    });
+    it('should allow the controller owner to add new markets', () => {
+      return controller.addMarket(token1._address)
+        .then(() => controller.eventualMarketListSize)
+        .then(marketListSize => {
+          expect(marketListSize).to.eq(1);
         });
     });
   });
