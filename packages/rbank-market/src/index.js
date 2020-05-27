c9177ddaaaabb0171c128669fc34a652da11de89
Mesi Rendon <amrendonsa@unal.edu.co>
2020-05-26 19:42:42 -0500

    Improving tests on market package
            I've made a lot of modifications mainly on the Market test and
            Market handler. This forced me to implement a basic token
            handler and its tests in order to make more sense of the Market
            handler. I also made an abstraction operation on the send method
            to send the transaction done by the selected metamask account,
            but it's useful on tests in order to select the account from
            ganache where the transaction is coming from.

---
+++ packages/rbank-market/src/index.js
@@ -0,0 +1,75 @@
+import MarketContract from './Market.json';
+import Token from './token';
+import { send, web3 } from '@rsksmart/rbank-utils';
+
+export default class Market {
+  constructor(address = '') {
+    if (!address.match(/0x[a-fA-F0-9]{40}/))
+      return new Error('Missing address');
+    this._instance = new web3.eth.Contract(MarketContract.abi, address);
+    this._address = address;
+    this._token = this._instance.methods.token()
+      .call()
+      .then(tokenAddress => new Token(tokenAddress));
+  }
+
+  get address() {
+    return this._address;
+  }
+
+  get eventualController() {
+    return new Promise((resolve, reject) => {
+      this._instance.methods.controller()
+        .call()
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  get eventualBaseBorrowRate() {
+    return new Promise((resolve, reject) => {
+      this._instance.methods.baseBorrowRate()
+        .call()
+        .then(baseBorrowRate => Number(baseBorrowRate))
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  setControllerAddress(controllerAddress) {
+    return new Promise((resolve, reject) => {
+      send(this._instance.methods.setController(controllerAddress))
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  supply(amount, from = '') {
+    return new Promise((resolve, reject) => {
+      this._token
+        .then(token => token.approve(this._address, amount, from))
+        .then(() => send(this._instance.methods.supply(amount), from))
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  static create(tokenAddress = '', baseBorrowRate) {
+    return new Promise((resolve, reject) => {
+      if (!tokenAddress.match(/0x[a-fA-F0-9]{40}/) || baseBorrowRate === undefined)
+        reject(new Error('Either the token address or the base borrow rate are missing'));
+      const market = new web3.eth.Contract(MarketContract.abi);
+      const deploy = market.deploy({
+        data: MarketContract.bytecode,
+        arguments: [tokenAddress, baseBorrowRate],
+      });
+      web3.eth.getAccounts()
+        .then(([from]) => [from, deploy.estimateGas({ from })])
+        .then(result => Promise.all(result))
+        .then(([from, gas]) => deploy.send({ from, gas }))
+        .then(instance => instance._address)
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+}
--- packages/rbank-market/test/market.spec.js
+++ packages/rbank-market/test/market.spec.js
@@ -1,7 +1,8 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import Web3 from 'web3';
-
+import FaucetTokenContract from '../../../dependencies/DeFiProt/build/contracts/FaucetToken.json';
+import ControllerContract from '../../../dependencies/DeFiProt/build/contracts/Controller.json';
 import Market from '../src';
 
 const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
@@ -11,12 +12,124 @@ chai.use(chaiAsPromised);
 const { expect } = chai;
 
 describe('Market handler', () => {
-  context('Initialization', () => {
-    it('should throw an error if no instance address is passed');
-  })
+  let controller;
+  let token;
+  let token1;
+  let market1;
+  let market1Address;
+  beforeEach(async () => {
+    const [owner] = await web3.eth.getAccounts();
+    const controllerContract = new web3.eth.Contract(ControllerContract.abi);
+    const controllerDeploy = controllerContract.deploy({
+      data: ControllerContract.bytecode,
+      arguments: [],
+    });
+    const controllerGas = await controllerDeploy.estimateGas({ from: owner });
+    controller = await controllerDeploy.send({ from: owner, gas: controllerGas });
+
+    token = new web3.eth.Contract(FaucetTokenContract.abi);
+    const deployToken1 = token.deploy({
+      data: FaucetTokenContract.bytecode,
+      arguments: [10000, 'TOK1', 0, 'TOK1'],
+    });
+    const gasToken1 = await deployToken1.estimateGas({ from: owner });
+    token1 = await deployToken1.send({ from: owner, gas: gasToken1 });
+    market1Address = await Market.create(token1._address, 10);
+    market1 = new Market(market1Address);
+  });
   context('Creation', () => {
-    it('should throw an error if neither the token address nor the base borrow rate are set');
-    it('should throw an error if it gets a no-token address');
-    it('should be linked to a controller');
+    it('should throw an error no token address is set', () => {
+      return expect(Market.create()).to.be.eventually.rejected;
+    });
+    it('should throw an error if no base borrow rate is set', () => {
+      return expect(Market.create(token1._address)).to.be.eventually.rejected;
+    });
+    it('should throw an error if an empty token address is set', () => {
+      return expect(Market.create('', 10)).to.be.eventually.rejected;
+    });
+    it('should throw an error if it gets a no-token address', () => {
+      return expect(Market.create(controller._address, 10))
+        .to.be.eventually.rejected;
+    });
+    it('should returns the market contract address after creation', () => {
+      return Market.create(token1._address, 10)
+        .then(marketAddress => {
+          expect(marketAddress).to.match(/0x[a-fA-F0-9]{40}/);
+        });
+    });
+    it('should get the instance smart contract address', () => {
+      return expect(market1.address).to.eq(market1Address);
+    });
+  });
+  context('Initialization', () => {
+    it('should throw an error if no instance address is passed', () => {
+      return expect(new Market()).to.be.an('error');
+    });
+    it('should return a valid market instance after passing a valid market address', () => {
+      return Market.create(token1._address, 10)
+        .then(marketAddress => {
+          expect(new Market(marketAddress)).not.be.an('error');
+        });
+    });
+    it('should be linked to a controller', () => {
+      return market1.setControllerAddress(controller._address)
+        .then(result => {
+          expect(result.transactionHash).to.match(/0x[a-fA-F0-9]{64}/);
+          return true;
+        })
+        .then(() => market1.eventualController)
+        .then(resolvedController => {
+          expect(resolvedController).to.eq(controller._address);
+        });
+    });
+    it('should have the same borrow rate from its creation', () => {
+      return market1.eventualBaseBorrowRate
+        .then(baseBorrowRate => {
+          expect(baseBorrowRate).to.eq(10);
+        });
+    });
+  });
+  context('Operational', () => {
+    let owner, user1, user2, user3, user4;
+    let token2;
+    let market2;
+    beforeEach(async () => {
+      [owner, user1, user2, user3, user4] = await web3.eth.getAccounts();
+
+      const deployToken2 = token.deploy({
+        data: FaucetTokenContract.bytecode,
+        arguments: [10000, 'TOK2', 0, 'TOK2'],
+      });
+      const gasToken2 = await deployToken2.estimateGas({ from: owner });
+      token2 = await deployToken2.send({ from: owner, gas: gasToken2 });
+      market2 = new Market(await Market.create(token2._address, 10));
+
+      await token1.methods.allocateTo(user1, 500).send({ from: user1 });
+    });
+    it('should throws an error if there is not enough balance to supply into a market', () => {
+      return expect(market1.supply(250, user2)).to.be.eventually.rejected;
+    });
+    it('should allow the current and funded user to supply into the market', () => {
+      const t1Allocate = token1.methods.allocateTo(owner, 500);
+      return t1Allocate.estimateGas({ from: owner })
+        .then(gas => t1Allocate.send({ from: owner, gas }))
+        .then(() => market1.supply(250))
+        .then(result => {
+          expect(result.transactionHash).to.match(/0x[a-fA-F0-9]{64}/);
+        });
+    });
+    it('should allow any user to supply token to the market', () => {
+      return market1.supply(250, user1)
+        .then(result => {
+          expect(result.transactionHash).to.match(/0x[a-fA-F0-9]{64}/);
+        });
+    });
+    it('should allow a second user borrowing from what was supplied by a first one');
+    it('should allow a second user to pay a borrowed amount');
+    it('should allow a first user to redeem tokens previously supplied into the market');
+    it('should throw an error on redeem if there is not enough supplied amount from the user');
+    it('should allow anyone to get the updatedSupplyOf value of any account');
+    it('should allow anyone to get the updatedBorrowedBy value of any account');
+    it('should allow anyone to get the current Market balance in its token terms');
   });
 });
